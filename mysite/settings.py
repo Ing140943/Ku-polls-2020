9065df4d9d57d341ac5124f850b551bd9b2d06fb
Ing140943 <skladee@hotmail.co.th>
2020-09-19 07:16:04 +0700

    Update path

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -27,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+    
 # Application definition
 
 INSTALLED_APPS = [
